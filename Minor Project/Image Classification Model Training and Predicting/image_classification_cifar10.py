91db963fb00debb048f05e17d0ad16f9e8c78875
AlphaBolt <2028014@kiit.ac.in>
2023-04-29 00:56:13 +0530

    Trying to add multiple models

--- Minor Project/Image Classification Model Training and Predicting/image_classification_cifar10.py
+++ Minor Project/Image Classification Model Training and Predicting/image_classification_cifar10.py
@@ -2,30 +2,38 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, models
 from keras.datasets import cifar10
-# idhar apan CIFAR-10 Data ko load krre
-(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
-# Data ko preprocess krlete hai 
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-# Defining the model
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
-#Compiling the model
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-#Training the model
-history = model.fit(train_images, train_labels, epochs=10, 
-                    validation_data=(test_images, test_labels))
-#Evaluating the modellll
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy:', test_acc)
-#Saving the model
-model.save('Minor Project\Image Classification Model Training and Predicting\saving_model\image_classification_model.h5')
+
+# Making 5 .h5 models to better improve accuracy of models
+
+for i in range(6,11):
+
+    # idhar apan CIFAR-10 Data ko load krre
+    (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+    # Data ko preprocess krlete hai 
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+    # Defining the model
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
+    #Compiling the model
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+    #Training the model
+    history = model.fit(train_images, train_labels, epochs=10, 
+                        validation_data=(test_images, test_labels))
+    #Evaluating the modellll
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print('Test accuracy:', test_acc)
+
+    #Saving the model
+    # This is the actual path
+    model_name = "saving_model\model"+str(i+1)+".h5"
+    model.save(model_name)
--- Minor Project/frontend-v2/app.py
+++ Minor Project/frontend-v2/app.py
@@ -4,8 +4,13 @@ import numpy as np
 import tensorflow as tf
 
 app = Flask(__name__)
-model = tf.keras.models.load_model('..\Image Classification Model Training and Predicting\saving_model\image_classification_model.h5')
 classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+# Making an array containing 5 models
+model = []
+for i in range(5):
+    model_name = "..\Image Classification Model Training and Predicting\saving_model\model"+str(i+1)+".h5"
+    model.append(tf.keras.models.load_model(model_name))
 
 @app.route("/")
 def index():
@@ -17,12 +22,30 @@ def predict():
     img = Image.open(request.files['image']).resize((32, 32))
     img = np.array(img) / 255.0
     img = np.expand_dims(img, axis=0)
-    # Make predictions
-    pred = model.predict(img)
-    pred_class = classes[np.argmax(pred)]
-    pred_prob = float(np.max(pred))
+
+    # Make predictions in all 5 models
+    pred = []
+    pred_class = []
+    pred_prob = []
+
+    for i in range(5):
+        pred.append(model[i].predict(img))
+        pred_class.append(classes[np.argmax(pred[i])])
+        pred_prob.append(float(np.max(pred[i])))
+        print("Probability:"+str(pred_prob[i])+", Class:"+pred_class[i])    
+
+    # Finding the best probability and storing its index
+    final_pred_class = pred_class[0]
+    final_pred_prob = pred_prob[0]
+
+    for i in range(1,5):
+        if pred_prob[i] > final_pred_prob:
+            final_pred_prob = pred_prob[i]
+            final_pred_class = pred_class[i]
+
+
     # Return the predictions as JSON
-    return jsonify({'class': pred_class, 'probability': pred_prob})
+    return jsonify({'class': final_pred_class, 'probability': final_pred_prob})
 
 if __name__ == '__main__':
     app.run(debug=True)
