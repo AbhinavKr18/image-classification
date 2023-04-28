3bbf9736b51f45b90674fd5cb7fa5319163dca21
Abhinav Kumar <abhinavkr18@outlook.com>
2023-04-28 22:47:42 +0530

    Changed the codes in files
    Changed the codes in given files:
    -> image_classification_cifar10.py
    -> predict_image.py
    
    Changes:
    Added the loop in image_classification_cifar10.py so that now it will generate 10 data models instead of one single model
    
    Edited the file predict_image.py so that now it will load 10 data models which we made above and now it will use the data model file with most accuracy. For Example
    a. image_classification_model_1.h5 : ACCURACY = 0.71
    b. image_classification_model_2.h5 : ACCURACY = 0.85
    c. image_classification_model_3.h5 : ACCURACY = 0.69
    
    so the code will use the file "image_classification_model_2.h5" since it have the most accuracy amongst the above 3 files.

--- Minor Project/Image Classification Model Training and Predicting/image_classification_cifar10.py
+++ Minor Project/Image Classification Model Training and Predicting/image_classification_cifar10.py
@@ -7,25 +7,27 @@ from keras.datasets import cifar10
 # Data ko preprocess krlete hai 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
+
+for i in range(10):
 # Defining the model
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
-#Compiling the model
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-#Training the model
-history = model.fit(train_images, train_labels, epochs=10, 
-                    validation_data=(test_images, test_labels))
-#Evaluating the modellll
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy:', test_acc)
-#Saving the model
-model.save('Minor Project\Image Classification Model Training and Predicting\saving_model\image_classification_model.h5')
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
+    #Compiling the model
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+    #Training the model
+    history = model.fit(train_images, train_labels, epochs=10, 
+                        validation_data=(test_images, test_labels))
+    #Evaluating the modellll
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print('Test accuracy:', test_acc)
+    #Saving the model
+    model.save('Minor Project\Image Classification Model Training and Predicting\saving_model\image_classification_model_{i}.h5')
--- Minor Project/Image Classification Model Training and Predicting/predict_image.py
+++ Minor Project/Image Classification Model Training and Predicting/predict_image.py
@@ -3,8 +3,12 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 from PIL import Image
-#loading the saved model
-model = keras.models.load_model('Minor Project\Image Classification Model Training and Predicting\saving_model\image_classification_model.h5')
+#loading the 10 models saved
+models = []
+for i in range(10):
+    model_path = f'Minor Project\Image Classification Model Training and Predicting\saving_model\image_classification_model_{i}.h5'
+    model = keras.models.load_model(model_path)
+    models.append(model)
 #loading the desired image
 image_path = input("Enter image path: ")
 image = Image.open(image_path)
@@ -12,9 +16,16 @@ image = image.resize((32, 32))
 image = np.array(image)
 image = image.astype('float32')
 image /= 255
-#making prediction
-prediction = model.predict(np.array([image]))
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 
-               'dog', 'frog', 'horse', 'ship', 'truck']
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+#Here we are now predicting the class for the selected image, by using the model having the most accuracy
+predictions = []
+for model in models:
+    prediction = model.predict(np.array([image]))
+    predictions.append(prediction)
+# choose the model with highest accuracy
+best_model_idx = np.argmax([pred[0][np.argmax(pred[0])] for pred in predictions])
+best_model = models[best_model_idx]
+# make prediction using the best model
+prediction = best_model.predict(np.array([image]))
 predicted_class = class_names[np.argmax(prediction)]
 print('Predicted class:', predicted_class)
